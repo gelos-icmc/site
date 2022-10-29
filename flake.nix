bc6d0a109208e304589456412a163f9d3d3838ee
Gabriel Fontes <hi@m7.rs>
2022-10-29 03:39:01 -0300

    flake: declarar cache binário

--- flake.nix
+++ flake.nix
@@ -8,24 +8,28 @@
 
 
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system};
-    in
-    rec {
-      packages = rec {
-        gelos-site = pkgs.callPackage ./default.nix { };
-        gelos-site-serve = pkgs.writeShellScriptBin "serve" ''
-          echo "Serving on http://localhost:8000"
-          ${pkgs.webfs}/bin/webfsd -F -f index.html -r ${gelos-site}/public
-        '';
-        default = gelos-site;
-      };
+  let pkgs = nixpkgs.legacyPackages.${system};
+  in rec {
+    packages = rec {
+      gelos-site = pkgs.callPackage ./default.nix { };
+      gelos-site-serve = pkgs.writeShellScriptBin "serve" ''
+        echo "Serving on http://localhost:8000"
+        ${pkgs.webfs}/bin/webfsd -F -f index.html -r ${gelos-site}/public
+      '';
+      default = gelos-site;
+    };
 
-      apps = rec {
-        gelos-site-serve = {
-          type = "app";
-          program = "${packages.gelos-site-serve}/bin/serve";
-        };
-        default = gelos-site-serve;
+    apps = rec {
+      gelos-site-serve = {
+        type = "app";
+        program = "${packages.gelos-site-serve}/bin/serve";
       };
-    });
+      default = gelos-site-serve;
+    };
+  }) // {
+    nixConfig = {
+      extra-substituers = [ "https://gelos-icmc.cachix.org" ];
+      extra-trusted-public-keys = [ "gelos-icmc.cachix.org-1:IQxtwf+SS2LUWWoPgzYQMAYUvsBA+7tdooE42KRcCWk=" ];
+    };
+  };
 }
