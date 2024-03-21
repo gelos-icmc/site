e249bbd9e70fe4a6974a62ab5d27cb6f0e10a8c9
Gabriel Fontes <hi@m7.rs>
2024-03-20 21:11:58 -0300

    compilar pdfs em paralelo

--- atas.nix
+++ atas.nix
@@ -11,9 +11,9 @@ stdenvNoCC.mkDerivation {
 
   buildPhase = ''
     for src_markdown in *-ata.md; do
-      echo "Building $src_markdown" >&2
-      pandoc -t beamer --shift-heading-level-by=-1 -V theme=gelos -V themeoptions=dark $src_markdown -o ''${src_markdown%-ata.md}.pdf
+      { pandoc -t beamer --shift-heading-level-by=-1 -V theme=gelos -V themeoptions=dark $src_markdown -o ''${src_markdown%-ata.md}.pdf && echo "Built $src_markdown"; } &
     done
+    wait
   '';
   installPhase = "mkdir -p $out && cp *.pdf $out/";
 }
