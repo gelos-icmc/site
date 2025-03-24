1a5768080e0b9d940178de572a29f0dad52df62e
Luana <luana@luana.dev.br>
2025-03-24 11:58:27 -0300

    Liability copypasta

--- src/assets/installfest/if6-downloadall.sh
+++ src/assets/installfest/if6-downloadall.sh
@@ -1,5 +1,10 @@
 ---
 ---
+# Copyright 2025 Luana Neder
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 #!/bin/sh
 
 set -e
@@ -8,6 +13,8 @@ LINKS="{%- for iso in site.data.if6.if6-isos -%}{{ iso["link"] }} {% endfor -%}"
 NAMES="{%- for iso in site.data.if6.if6-isos -%}{{ iso["filename"] }} {% endfor -%}"
 
 set -- $NAMES
+
+echo "Este software é fornecido \"da forma em que se encontra\", sem quaisquer garantias, expressas ou implícitas, incluindo, mas não se limitando a, garantias de comercialização, adequação a um propósito específico e não violação de direitos. Em nenhuma hipótese os autores ou colaboradores serão responsáveis por quaisquer danos diretos, indiretos, incidentais, especiais, exemplares ou consequenciais (incluindo, mas não se limitando a, aquisição de bens ou serviços substitutos, perda de uso, dados ou lucros, ou interrupção de negócios), causados de qualquer forma e sob qualquer teoria de responsabilidade, seja por contrato, responsabilidade objetiva ou ato ilícito (incluindo negligência ou outra forma), decorrentes do uso deste software, mesmo que avisados da possibilidade de tais danos."
 
 echo ⚠️ ATENÇÃO: Este script irá baixar $# isos, chegando a aproximadamente 50GB. Tenha certeza de que quer fazer isso ou cancele imediatamente.
 echo Os downloads serão feitos no diretório atual, $PWD
